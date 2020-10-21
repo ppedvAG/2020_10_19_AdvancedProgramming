2a8188d9d65f31729349829de52f754212f7226f
Kevin Winter <KevinW@ppedv.de>
2020-10-21 16:28:38 +0200

    Reflection Plugin Pattern
    Link-Collections

---
+++ AdvancedProgramming_2020_10_19/HelloReflections/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Reflection;
+
+namespace HelloReflections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Assembly geladen = Assembly.LoadFrom("netcoreapp3.1\\TrumpTaschenrechner.dll");
+
+            var allTypes = geladen.GetTypes(); // Den Typ ermitteln
+
+            Type TaschnrechnerTyp = geladen.GetType("TrumpTaschenrechner.Taschnrechner");
+
+            object tr = Activator.CreateInstance(TaschnrechnerTyp);
+
+            MethodInfo addInfo = TaschnrechnerTyp.GetMethod("Add", new Type[] { typeof(Int32), typeof(Int32) });
+
+            var result = addInfo.Invoke(tr, new object[] { 6, 4 });
+            Console.WriteLine(result);
+
+            Console.ReadLine();
+        }
+    }
+}
